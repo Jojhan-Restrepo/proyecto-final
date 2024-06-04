b137e579c069a46fe51510f8b997dcdb49455a6a
Jojhan-Restrepo <161488744+Jojhan-Restrepo@users.noreply.github.com>
2024-06-04 00:30:19 -0500

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,69 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QTimer>
+#include "Particula.h"
+#include "Jugador.h"
+#include "enemigos.h"
+#include "Bala.h"
+#include "trampas.h"
+#include "trampas2.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+    void startGame();
+    void showScores();
+    void updateGame();
+    void resetGame();
+    void returnToMenu();
+    void modifyTime(); // Nueva función para modificar el tiempo de los fenómenos físicos
+    void animacionTiroDesdeEnemigo();
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene *menuScene;
+    QGraphicsScene *gameScene;
+    QTimer *timer;
+    Particula *bola;
+    Jugador *jug1;
+    trampas2 *trampa2;
+    QVector<Enemigos*> enemigos;
+    QVector<Trampas*> trampas;
+    QVector<Jugador*> jugador;
+    QVector<QGraphicsRectItem*> obst;
+    qreal tiempoTrans;
+    bool clic;
+    bool jugadorVivo; // Variable para rastrear el estado del jugador
+    QString playerName;
+
+    void initGameScene();
+    void movjug(Jugador *jug1);
+    void hmov(Particula *bola);
+    void enemigomov(Enemigos *enem1);
+    void animacionTiro(const QPointF &posTiro);
+    void incrementarContador();
+    void Trampa(Trampas *trampa);
+    void mousePressEvent(QMouseEvent *event) override; // Reimplementación de mousePressEvent
+    void clearGameOverScene();
+    void writeScoreToFile(const QString& name, int score);
+
+signals:
+    void jugadorMuerto();
+};
+
+#endif // MAINWINDOW_H
